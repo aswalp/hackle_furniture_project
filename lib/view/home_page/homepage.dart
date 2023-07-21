f36a55cd242061bb9819506eefd3771703b21389
aswal <aswalpt@gmail.com>
2023-07-21 15:55:41 +0530

    home page commit

---
+++ lib/view/home_page/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainHomePageUi extends StatelessWidget {
+  const MainHomePageUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // backgroundColor: ,
+
+        // body: ,
+
+        );
+  }
+}
